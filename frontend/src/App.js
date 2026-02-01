64391c53fb1052f5056575111d9b18b961374b8d
nurzhan <nurzhanduisenbaev480@mail.ru>
2026-02-01 20:31:48 +0500

    fix 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     setResponse("");
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/api/chat", {
+      const res = await fetch("https://text-generator-gskh.vercel.app/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text })
